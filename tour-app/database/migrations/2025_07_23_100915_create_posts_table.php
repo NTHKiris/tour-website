e28b52354a977e113f4c8cf9716537fd64d64817
kingofgame1000 <nguyenhoang02012004@gmail.com>
2025-07-24 10:58:25 +0700

    fix: database

--- tour-app/database/migrations/2025_07_23_100915_create_posts_table.php
+++ tour-app/database/migrations/2025_07_23_100915_create_posts_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -16,7 +15,7 @@ return new class extends Migration
             $table->string('title'); // Tiêu đề bài viết
             $table->integer('subscribers')->default(0); // Số lượng đăng ký (hoặc có thể là trạng thái)
             $table->string('link'); // Đường dẫn bài viết
-            $table->foreignId('category_id')->constrained('category_posts')->onDelete('cascade'); // Khóa ngoại tới category_posts
+            $table->foreignId('category_id')->constrained('post_categories')->onDelete('cascade'); // Khóa ngoại tới category_posts
             $table->foreignId('author_id')->constrained('users')->onDelete('cascade'); // Khóa ngoại tới users
             $table->timestamps(); // Thời gian tạo và cập nhật
         });
