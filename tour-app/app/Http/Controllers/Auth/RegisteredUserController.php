cac370aee14bf1b3b05a975ac32bb73b920c7d8d
kingofgame1000 <nguyenhoang02012004@gmail.com>
2025-07-24 10:43:16 +0700

    add: add table user

--- tour-app/app/Http/Controllers/Auth/RegisteredUserController.php
+++ tour-app/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -29,16 +29,18 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
-        $request->validate([
+        $validated = $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'phone' => ['required', 'string']
         ]);
 
         $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+            'phone' => $validated['phone'],
         ]);
 
         event(new Registered($user));
