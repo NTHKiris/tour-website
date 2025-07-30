5dc8f52993b1894b0dc1f65ba80cef1ccfd02aea
kingofgame1000 <nguyenhoang02012004@gmail.com>
2025-07-30 16:48:44 +0700

    add: add section hero for homepage

--- tour-app/resources/views/home.blade.php
+++ tour-app/resources/views/home.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.tour')
 
-@section('title', 'Trang chủ | Bình Định Tour - Khám phá vẻ đẹp Bình Định')
-@section('description', 'Khám phá những điểm đến tuyệt vời tại Bình Định với các tour du lịch chất lượng cao. Kỳ Co, Eo Gió, Quy Nhon và nhiều địa điểm hấp dẫn khác.')
+@section('title', 'Trang chủ | Quy Nhơn Tour - Khám phá vẻ đẹp Quy Nhơn')
+@section('description', 'Khám phá những điểm đến tuyệt vời tại Quy Nhơn với các tour du lịch chất lượng cao. Kỳ Co, Eo Gió, Quy Nhon và nhiều địa điểm hấp dẫn khác.')
 
 @section('content')
 
-
-
+    <x-hero-section title="Trọn niềm vui với những chuyến đi đáng nhớ"
+        description="Quy Nhơn - thành phố huyền thoại với bãi biển trong xanh, Eo Gió hùng vĩ và tháp Chăm cổ kính. Cùng khám phá vẻ đẹp hoang sơ và tận hưởng những trải nghiệm tuyệt vời nơi đây!"
+        image1="/images/ky-co.jpg" image2="/images/eo-gio.jpg" image3="/images/quy-nhon.jpg" />
 @endsection
