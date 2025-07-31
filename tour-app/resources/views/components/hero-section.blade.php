66a268f49e7a6e784ff5bf1a6d83b36268a8d492
kingofgame1000 <nguyenhoang02012004@gmail.com>
2025-07-31 08:54:18 +0700

    add: add hero section for hommepage

--- tour-app/resources/views/components/hero-section.blade.php
+++ tour-app/resources/views/components/hero-section.blade.php
@@ -12,7 +12,7 @@
                 <h1 class="text-4xl lg:text-6xl font-bold italic leading-tight mb-6 text-cyan-600" >{{$title}}</h1>
                 <p class="text-gray-600 text-lg leading-relaxed mb-8"  >{{$description}}</p> 
                 
-                <button class="group relative overflow-hidden rounded-lg py-4 font-medium transition-all duration-350 text-[16px] px-8 bg-cyan-500 text-white hover:text-black-900 flex justify-center items-center gap-2 hover:text-cyan-500 ">
+                <button class="group relative overflow-hidden rounded-lg py-4 font-medium transition-all duration-350 text-lg px-8 bg-cyan-500 text-white hover:text-black-900 flex justify-center items-center gap-2 hover:text-cyan-500 ">
                     <span class="relative z-20 flex justify-center items-center ">          
                         <span>Khám phá ngay</span>     
                     </span>
@@ -20,13 +20,13 @@
                 </button>
             </div>
             <div class="grid grid-cols-2 grid-rows-2 gap-4 h-[500px]">
-                <div class="row-span-2 rounded-3xl overflow-hidden shadow-2xl border-4 border-cyan-600">
+                <div class="row-span-2 rounded-3xl overflow-hidden shadow-2xl border-4 border-cyan-600 hover:scale-110  duration-700 ">
                     <img src="{{$image2}}" alt="" class="w-full h-full object-cover">
                 </div>
-                <div class="rounded-3xl overflow-hidden shadow-2xl border-4 border-cyan-600">
+                <div class="rounded-3xl overflow-hidden shadow-2xl border-4 border-cyan-600 hover:scale-110 duration-700">
                     <img src="{{$image1}}" alt="" class="w-full h-full object-cover">
                 </div>
-                <div class="rounded-3xl overflow-hidden shadow-2xl border-4 border-cyan-600" >
+                <div class="rounded-3xl overflow-hidden shadow-2xl border-4 border-cyan-600 hover:scale-110 duration-700" >
                     <img src="{{$image3}}" alt="" class="w-full h-full object-cover">
                 </div>
             </div>
